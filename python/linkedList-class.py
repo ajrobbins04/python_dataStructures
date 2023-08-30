084ad276133cb088365078ec0ea51ded86ef8638
ajrobbins04 <ajrobbins04@gmail.com>
2023-08-29 19:34:55 -0700

    linked list class almost done

--- python/linkedList-class.py
+++ python/linkedList-class.py
@@ -47,8 +47,8 @@ class LinkedList:
         # If the the list is not empty, then only
         # self.head will be affected by the insertion.
         else:
-            new_node.next = self.head  # Connect the new node to the current head using "next" link
-            self.head.prev = new_node  # Connect the current head to the new node using the "prev" link
+            new_node.next = self.head  # Connect the new node to the curr_node head using its 'next' link
+            self.head.prev = new_node  # Connect the curr_node head to the new node using its 'prev' link
             self.head = new_node       # Now set the head equal to the new node
 
     def insert_tail(self, value):
@@ -69,8 +69,8 @@ class LinkedList:
         # If the the list is not empty, then only
         # self.tail will be affected by the insertion.
         else:
-            new_node.prev = self.tail  # Connect the new node to the current tail using "prev" link
-            self.head.next = new_node  # Connect the current tail to the new node using the "next" link
+            new_node.prev = self.tail  # Connect the new node to the curr_node tail using its 'prev' link
+            self.head.next = new_node  # Connect the curr_node tail to the new node using its 'next' link
             self.tail = new_node       # Now set the tail equal to the new node
 
     def remove_head(self):
@@ -86,8 +86,8 @@ class LinkedList:
 
         # Removal only applies to the head when there's more than one node.
         else:
-            secondNode = self.head.next  # The head node's next link provides the second node.
-            secondNode.prev = None       # Disconnect the second node from the head by setting its prev link to null.
+            secondNode = self.head.next  # The head node's 'next' link provides the second node.
+            secondNode.prev = None       # Disconnect the second node from the head by setting its 'prev' link to null.
             self.head = secondNode       # Now set the head to the second node.
 
     def remove_tail(self):
@@ -103,6 +103,80 @@ class LinkedList:
 
         # Removal only applies to the tail when there's more than one node.
         else:
-            secondToLastNode = self.tail.prev  # The tail node's prev link provides the second to last node.
-            secondToLastNode.next = None       # Disconnect the second to last node from the tail by setting its prev link to null.
+            secondToLastNode = self.tail.prev  # The tail node's 'prev' link provides the second to last node.
+            secondToLastNode.next = None       # Disconnect the second to last node from the tail by setting its 'prev' link to null.
             self.tail = secondToLastNode       # Now set the tail to the second to last node.
+    
+    def insert_after(self, value, new_value):
+        """
+        Inserts 'new_value' after the first occurence
+        of 'value' in the linked list.
+        """
+        # Begin searching for the node that holds 
+        # 'value' at the head of the list.
+        curr_node = self.head
+
+        while curr_node is not None:
+            if curr_node.data == value:
+                # if current node is also the tail node, then call insert_tail().
+                if curr_node == self.tail:
+                    self.insert_tail(new_value)
+                # If value is found in any other current node, then it will create
+                # and insert the new node at the next node.
+                else: 
+                    new_node = LinkedList.Node(new_value)
+                    new_node.prev = curr_node   # Connect the new node's 'prev' link to the current node.
+                    next_node = curr_node.next  # Get the next node after current node from its 'next' link.
+                    new_node.next = next_node   # Connect the new node's 'next' link to the next node.
+                    next_node.prev = new_node   # Connect the next node's 'prev' link to the new node.
+                    next_node = new_node        # Set the next node equal to the new node.
+                
+                # Can exit the function once new node is inserted.
+                return 
+            
+            # Go to the next node to search for 'value'.
+            curr_node = next_node
+
+    def remove(self, value):
+        """
+        Removes the first node found that contains 'value'.
+        """
+        # Begin searching for the node that holds 
+        # 'value' at the head of the list.
+        curr_node = self.head
+
+        # If the head node contains 'value',
+        # then call remove_head()
+        if curr_node.data == value:
+            self.remove_head()
+
+        else:
+            # Search for value until it is found,
+            # or the end of the list is reached.
+            while curr_node is not None:
+                if curr_node.data == value:
+                    # If the tail node contains 'value',
+                    # then call remove_tail()
+                    if curr_node == self.tail:
+                        self.remove_tail()
+
+                    # If value is found in any other current node,
+                    # then remove the current node.
+                    else:
+                        """
+                        The following two lines of code could be used
+                        instead of the code on lines 176 - 180:
+
+                        curr_node.next.prev = curr_node.prev
+                        curr_node.prev.next = curr_node.next
+
+                        However, the extra lines of code are used to make the
+                        action of removing the current node less convoluted.
+                        """
+                        prev_node = curr_node.prev  # Get the prev node before current node from its 'prev' link.
+                        next_node = curr_node.next  # Get the next node after current node from its 'next' link.
+                        
+                        prev_node.next = curr_node.next # Set the previous node's 'next' link to the curent node's 'next' link.
+                        next_node.prev = curr_node.prev # Set the next node's 'prev' link to the current node's 'prev' link.
+                        
+
